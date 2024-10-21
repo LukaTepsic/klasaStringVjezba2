8d3c92596f9bf9ae6a0b39b645efe76dbb8e4343
LukaTepsic <166146790+LukaTepsic@users.noreply.github.com>
2024-10-21 13:40:41 +0200

    Add files via upload

---
+++ klasaStringVjezba2/klasaStringVjezba2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaStringVjezba2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite niz znakova:");
+            string input = Console.ReadLine();
+            string output = input.Replace(" ", "_");
+            Console.WriteLine("Rezultat: " + output);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
